6648f70848bdd3dce73def953c258b5cd7b46bba
sunflower0309 <805786665@qq.com>
2020-08-05 12:53:34 -0700

    remote deploy test

--- src/initial.py
+++ src/initial.py
@@ -1,6 +1,6 @@
-from src import buildProj,workerServer
+from src import buildProj,workerServer,utils
 
 
-workerServer.operate_proj('https://github.com/sunflower0309/gson.git','ebd311ff5467a4909163a20212a284bcf1824023','2020-07-29T09:59:24Z')
-workerServer.operate_proj('https://github.com/sunflower0309/gson.git','acb8361f7a0bbf399b9ec81beb219cc40b22a07c','2020-07-29T07:28:50Z')
-workerServer.operate_proj('https://github.com/sunflower0309/jsoup.git','dc38b0aed68f0ece00a32dd927e56c4e50132ed9','2020-07-20T21:36:02Z')
+workerServer.operate_proj('https://github.com/sunflower0309/gson.git','ebd311ff5467a4909163a20212a284bcf1824023',utils.githubTimeConvert('2020-07-29T09:59:24Z'))
+workerServer.operate_proj('https://github.com/sunflower0309/gson.git','acb8361f7a0bbf399b9ec81beb219cc40b22a07c',utils.githubTimeConvert('2020-07-29T07:28:50Z'))
+workerServer.operate_proj('https://github.com/sunflower0309/jsoup.git','dc38b0aed68f0ece00a32dd927e56c4e50132ed9',utils.githubTimeConvert('2020-07-20T21:36:02Z'))
